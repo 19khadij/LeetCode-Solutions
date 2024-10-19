fcaa40d5d6fc3fb2190427692edb4e59a4c4ab51
19khadij <khad191919@gmail.com>
2024-10-19 19:34:08 +0500

    Add file TwoSum

---
+++ TwoSum.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+Time Complexity is O(n^2)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+                
+Time Complexity is O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}  
+        for i, num in enumerate(nums):
+            complement = target - num  
+            if complement in seen:  
+                return [seen[complement], i]  
+            seen[num] = i  
+
+sum=Solution()
+result=sum.twoSum([2,3,4,5],9)
+print(result)
